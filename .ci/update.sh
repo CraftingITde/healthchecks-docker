c14e7f3c231b9da95fd4867c783e6ffed9c7cbe3
KStrüssmann <kstruessmann@craftingit.de>
2020-03-02 17:34:58 +0100

    Auto Update

---
+++ .ci/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+LATEST_VERSION=$(curl -s 'https://api.github.com/repos/healthchecks/healthchecks/releases/latest' | jq -r '.tag_name');
+CURRENT_VERSION=$(sed -nr 's/ARG HEALTHCHECKS_VERSION=*(.+)/\1/p' Dockerfile);
+REPO_ADRESS=${git config --get remote.origin.url | sed 's/https\?:\/\///'}
+
+echo "Found versions:"
+echo "  Latest:  ${LATEST_VERSION}"
+echo "  Current: ${CURRENT_VERSION}"
+echo "  Repo:    ${REPO_ADRESS}"
+
+sed --in-place "s/HEALTHCHECKS_VERSION=${CURRENT_VERSION}/HEALTHCHECKS_VERSION=${LATEST_VERSION}/g" Dockerfile
+
+git diff --exit-code . > /dev/null
+
+if [ "$?" -eq "0" ]; then
+	echo "No changes made. Nothig to do..."
+    exit 0
+else
+	echo "Committing changes"
+#	add changes and Push
+    git config user.name "CraftingIT-Bot"
+    git config user.email "$username@craftingit.de"
+    git add Dockerfile
+	git commit --message "Bump to latest version ${VERSION}"
+    git push https://$username:$password@$REPO_ADRESS
+fi
+
+git show "${LATEST_VERSION}" &> /dev/null
+if [ "$?" -eq "0" ]; then
+	echo "Tag '${LATEST_VERSION}' already exist. Skipping..."
+else
+
+	echo "Tag '${LATEST_VERSION}' not exist. Creating..."
+    git tag -a ${LATEST_VERSION} -m "New Version ${LATEST_VERSION} by CraftingIT-Bot"
+
+    git push https://$username:$password@$REPO_ADRESS --tags
+fi
