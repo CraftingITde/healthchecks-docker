6178b58f4f397992018e84ddcf102d5bd491de70
Kai Strüssmann <kai.struessmann@timbertec.com>
2025-04-01 13:28:54 +0200

    Update nightly-daemon.sh

--- container-fs/nightly-daemon.sh
+++ container-fs/nightly-daemon.sh
@@ -2,11 +2,9 @@
 while true; do
     echo "Running nightly maintenance tasks..."
     cd /app
-    ./manage.py prunepings
-    ./manage.py prunenotifications
-    ./manage.py pruneusers
+    ./manage.py pruneobjects
     ./manage.py prunetokenbucket
-    ./manage.py pruneflips
+    ./manage.py pruneusers
     ./manage.py prunepingsslow
     echo "Nightly maintenance completed. Sleeping for 24 hours..."
     sleep 86400 # 24 Stunden = 86400 Sekunden
